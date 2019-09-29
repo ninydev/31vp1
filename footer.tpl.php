ad3fb9ff569bf83298e263148fa46761f474e00d
Olexander Nykytin <keeper@ninydev.com>
2019-09-29 13:27:38 +0300

    29-09

---
+++ footer.tpl.php
@@ -0,0 +1,3 @@
+<footer>
+&copy Olexander Nykytin 
+</footer>
--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
+require_once ("vendor/autoload.php");
+
+
+require_once ("header.tpl.php");
+
+require_once ("header.tpl.php");
+
 echo "Hello World";
+include ("second.php");
+
+include ("footer.tpl.php");
